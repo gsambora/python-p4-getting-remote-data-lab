81b081c4fb2fb98f3b0a96f1d571661b2d55a102
Hana Sambora <grace.sambora@gmail.com>
2024-02-18 14:01:02 -0600

    Complete walkthrough

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        data_list = []
+        content = json.loads(self.get_response_body())
+        for item in content:
+            data_list.append(item)
+
+        return data_list
